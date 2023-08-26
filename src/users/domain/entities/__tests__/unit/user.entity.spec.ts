94499a6bd98bf5374a30b7ee6a8badd142851c1c
Junior5801 <junior_silvapereira@hotmail.com>
2023-08-26 18:26:28 -0300

    feat: getters methods unit tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -21,4 +21,27 @@ describe('User entity unit test', () => {
     expect(sut.props.password).toBe(props.password);
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
+
+  it('should be able to return getter field', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toEqual(props.name);
+    expect(typeof sut.props.name).toBe('string');
+  });
+
+  it('should be able to return getter field', () => {
+    expect(sut.props.email).toBeDefined();
+    expect(sut.props.email).toEqual(props.email);
+    expect(typeof sut.props.email).toBe('string');
+  });
+
+  it('should be able to return getter field', () => {
+    expect(sut.props.password).toBeDefined();
+    expect(sut.props.password).toEqual(props.password);
+    expect(typeof sut.props.password).toBe('string');
+  });
+
+  it('should be able to return getter field', () => {
+    expect(sut.props.createdAt).toBeDefined();
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
 });
