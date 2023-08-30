6e103a564091fb030dd4959c58fa7d7f9bd1ee2b
Junior5801 <junior_silvapereira@hotmail.com>
2023-08-29 22:45:56 -0300

    ++ find by id method

--- src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -1,5 +1,6 @@
 import { Entity } from '@/shared/domain/entities/entity';
 import { InMemoryRepository } from '../../in-memory.repository';
+import { NotFoundError } from '@/shared/domain/errors/not-found-errors';
 
 type StubEntityProps = {
   name: string;
@@ -21,5 +22,19 @@ describe('Repository unit tests', () => {
     const entity = new StubEntity({ name: 'user1', price: 1 });
     await sut.insert(entity);
     expect(entity.toJSON()).toStrictEqual(sut.items[0].toJSON());
+  });
+
+  it('Should be to throw new error when entity not found', async () => {
+    await expect(sut.findById('fakeID')).rejects.toThrow(
+      new NotFoundError('Entity not found.'),
+    );
+  });
+
+  it('Should be able to find an entity by id', async () => {
+    const entity = new StubEntity({ name: 'user1', price: 1 });
+    await sut.insert(entity);
+    const result = await sut.findById(entity._id);
+
+    expect(entity.toJSON()).toStrictEqual(result.toJSON());
   });
 });
