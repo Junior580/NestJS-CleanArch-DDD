807f50a8e873aeeecd195a36b355dfdfcb208265
Junior5801 <junior_silvapereira@hotmail.com>
2023-09-11 00:04:07 -0300

    ++refactor use repository pt-3

--- src/users/domain/repositories/user.repository.ts
+++ src/users/domain/repositories/user.repository.ts
@@ -1,8 +1,25 @@
-import { SearchableRepositoryInterface } from '@/shared/domain/repositories/searchable-repository-contracts';
+import {
+  SearchParams as defaultSearchParams,
+  SearchResult as defaultSearchResult,
+  SearchableRepositoryInterface,
+} from '@/shared/domain/repositories/searchable-repository-contracts';
 import { UserEntity } from '../entities/user.entity';
 
-export interface UserRepository
-  extends SearchableRepositoryInterface<UserEntity, any, any> {
-  findByEmail(email: string): Promise<UserEntity>;
-  emailExists(email: string): Promise<void>;
+export namespace UserRepository {
+  export type Filter = string;
+
+  export class SearchParams extends defaultSearchParams<Filter> {}
+
+  export class SearchResult extends defaultSearchResult<UserEntity, Filter> {}
+
+  export interface Repository
+    extends SearchableRepositoryInterface<
+      UserEntity,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {
+    findByEmail(email: string): Promise<UserEntity>;
+    emailExists(email: string): Promise<void>;
+  }
 }
